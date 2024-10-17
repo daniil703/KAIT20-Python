2ce9ceb9b263c59d2abcf1e5b5abdb18aa8685ea
daniil703 <60955851+daniil703@users.noreply.github.com>
2024-10-17 10:15:46 +0300

    Add files via upload

---
+++ Python-Data-types nalimov.py
@@ -0,0 +1,34 @@
+integer_var = 42 #задание 1
+float_var = 3.14
+string_var = "Hello, World!"
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (42, 3.14, "Barash", True)
+dict_var = {"name": "Daniil", "age": 17, "city": "Surgut"}
+set_var = {1, 2, 3, 4, 5}
+bool_var = True
+
+print("integer_var:", integer_var)
+print("float_var:", float_var)
+print("string_var:", string_var)
+print("list_var:", list_var)
+print("tuple_var:", tuple_var)
+print("dict_var:", dict_var)
+print("set_var:", set_var)
+print("bool_var:", bool_var)
+
+name = "Даня" #задание 2
+age = 17
+average_grade = 4.7
+favorite_movies = ["Побег из Шоушенка", "Драйв", "Властелин колец"]
+personal_info = ("Налим", 123, 4.5)
+skills = {"Python": 4, "SQL": 3, "Machine Learning": 2}
+visited_countries = {"Россия", "Италия", "Испания"}
+has_driver_license = True
+
+ 
+ 
+processor_name = "Intel Core i5-8400H"
+ram_size = 16  # ГБ
+internet_speed = 600  # Мбит/с
+system_info = [processor_name, ram_size, internet_speed]
+print(system_info)
